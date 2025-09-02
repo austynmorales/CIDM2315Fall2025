f4429eb0d02264d969219f054de0e1c58e14b61f
Austyn Morales <amorales14@buffs.wtamu.edu>
2025-09-01 23:55:49 -0500

    Update HomeWork1.cs.cs
    forgot to input homework from visual studio

--- HomeWork1.cs.cs
+++ HomeWork1.cs.cs
@@ -5,8 +5,17 @@ class Program
     static void Main(string[] args)
     {
         // homework 1 code
-        Console.WriteLine("Answer of Homework1");
+        x = 2.5;
+        y = 3.3;
+
+        # Calculate z using the equation: z = 4**+3y;
+
+        z = 4 * x**2 + 3 * y;
+
+        # Print the result
+        Console.WriteLine("The value of z is:", z);
         // new code of homework1
     }
 }
+
 
